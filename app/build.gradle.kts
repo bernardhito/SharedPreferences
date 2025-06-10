dd89cb0eb2f46546f5569ee7be0c304c6ad94a86
Bernardo <befreitas00@gmail.com>
2025-06-10 19:51:53 -0300

    sla q djabo nao ta funcionando

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,20 +27,23 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
+        viewBinding = true
         compose = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,11 +53,14 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
--- app/src/main/java/com/bernardofreitas/etimsharedpreferences/MainActivity.kt
+++ app/src/main/java/com/bernardofreitas/etimsharedpreferences/MainActivity.kt
@@ -2,24 +2,92 @@ package com.bernardofreitas.etimsharedpreferences
 
 import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.bernardofreitas.etimsharedpreferences.ui.theme.ETIMSharedPreferencesTheme
+import com.bernardofreitas.etimsharedpreferences.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity (){
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    companion object {
+        const val NOME_ARQUIVO = "arquivo_prefs.xml"
+    }
+
+    private var cor = ""
+
+    private fun salvarCor(cor: String) {
+        val prefs = getSharedPreferences(NOME_ARQUIVO, MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putString("Cor", cor)
+        editor.apply()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activitymain)
-        supportActionBar!!.hide()
-        window.statusBarColor= Color.WHITE
+
+        supportActionBar?.hide()
+        window.statusBarColor = Color.WHITE
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cor1.setOnClickListener {
+            cor = "#FF3700B3"
+            salvarCor(cor)
+            snackBar(it)
+        }
+
+        binding.cor2.setOnClickListener {
+            cor = "#FF03DAC5"
+            salvarCor(cor)
+            snackBar(it)
+        }
+
+        binding.cor3.setOnClickListener {
+            cor = "#F44336"
+            salvarCor(cor)
+            snackBar(it)
+        }
+
+        binding.cor4.setOnClickListener {
+            cor = "#4CAF5B"
+            salvarCor(cor)
+            snackBar(it)
+        }
+
+        binding.cor5.setOnClickListener {
+            cor = "#FFC107"
+            salvarCor(cor)
+            snackBar(it)
+        }
+
+        binding.btnTrocar.setOnClickListener {
+            val prefs = getSharedPreferences(NOME_ARQUIVO, MODE_PRIVATE)
+            val cor = prefs.getString("Cor", "")
+            if (!cor.isNullOrEmpty()) {
+                binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+            } else {
+                Toast.makeText(this, "Nenhuma cor salva", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val preferencias = getSharedPreferences(NOME_ARQUIVO, MODE_PRIVATE)
+        val cor = preferencias.getString("Cor", "")
+        if (!cor.isNullOrEmpty()) {
+            binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+        }
+    }
+
+    private fun snackBar(view: View) {
+        val snackbar = Snackbar.make(view, "Cor de fundo alterada com sucesso!", Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("OK") { }
+        snackbar.setActionTextColor(Color.BLUE)
+        snackbar.setBackgroundTint(Color.LTGRAY)
+        snackbar.setTextColor(Color.GREEN)
+        snackbar.show()
     }
 }
