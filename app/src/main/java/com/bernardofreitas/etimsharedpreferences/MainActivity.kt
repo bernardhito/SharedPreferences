5e97fa86df980a03e1bd4df809278147443ce4cd
Bernardo Lima <befreitas00@gmail.com>
2025-06-10 10:49:32 -0300

    whats my name

--- app/src/main/java/com/bernardofreitas/etimsharedpreferences/MainActivity.kt
+++ app/src/main/java/com/bernardofreitas/etimsharedpreferences/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.bernardofreitas.etimsharedpreferences
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,35 +15,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bernardofreitas.etimsharedpreferences.ui.theme.ETIMSharedPreferencesTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity (){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ETIMSharedPreferencesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ETIMSharedPreferencesTheme {
-        Greeting("Android")
+        setContentView(R.layout.activitymain)
+        supportActionBar!!.hide()
+        window.statusBarColor= Color.WHITE
     }
 }
